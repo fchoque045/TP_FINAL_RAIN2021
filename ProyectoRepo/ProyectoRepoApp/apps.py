0affd00746667d9e82fd69fd6b3491ab09a0a5ef
Fabián Choque <fabianchoque28@gmail.com>
2021-06-21 00:35:10 -0300

    Delete ProyectoRepo directory

--- ProyectoRepo/ProyectoRepoApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProyectorepoappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ProyectoRepoApp'
--- ProyectoRepo/ProyectoRepoApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from ProyectoRepoApp import views
-
-urlpatterns = [
-    path('', views.home,name="home"),
-    path('registro/<int:pk>', views.registro, name="registro"),
-]
